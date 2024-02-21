cf128e993ec3ecfa5dad6276d5c68ab95c243a91
dvcdung <dvcvochidung@gmail.com>
2024-02-21 14:30:14 +0700

    John changed

---
+++ bats/unity.bat
@@ -0,0 +1,7 @@
+@echo off
+
+start "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Unity Hub.lnk"
+start code --reuse-window
+start chrome "https://learn.unity.com/tutorial/lesson-3-1-jump-force?uv=2022.3&pathwayId=5f7e17e1edbc2a5ec21a20af&missionId=5f7648a4edbc2a5578eb67df&projectId=5cf9639bedbc2a2b1fe1e848#658f43baedbc2a23f721442c" "https://chat.openai.com/" "https://www.youtube.com/watch?v=L-mLsFt5ZpI"
+
+:End
