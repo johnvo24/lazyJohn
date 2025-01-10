b2f08c2a8e8bf34e540443f1e79a64fab623c993
Johnvo-PopOS <johnvo@jv-popos.www.tendawifi.com>
2025-01-10 16:51:26 +0700

    John updated

--- linux_script_shells/setup.sh
+++ linux_script_shells/setup.sh
@@ -8,6 +8,18 @@ sudo dnf update
 sudo dnf install ibus-unikey
 ibus restart # Then restart the laptop
 
-# More 
+# Fix Dual-Boot Menu
+# sudo apt install os-prober
+# sudo os-prober
+# sudo mount /dev/sdb1 /mnt # Change sdb1 to partition name of Window boot
+# sudo cp -ax /mnt/EFI/Microsoft /boot/efi/EFI
+# sudo reboot # Then spam the spacebar to enter the boot menu
+
+# Nodejs install v22.13.0 (LTS)
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+# Download and install Node.js:
+nvm install 22
+
 
 exit 0
